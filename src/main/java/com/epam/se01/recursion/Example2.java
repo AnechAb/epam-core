5ba95529a8ae6ce9e63ed60c5d052ddc51a864cb
AnechAb <abubekirova@inbox.ru>
2022-08-05 12:02:50 +0300

    Code from Lesson 12

---
+++ src/main/java/com/epam/se01/recursion/Example2.java
@@ -0,0 +1,16 @@
+package com.epam.se01.recursion;
+
+public class Example2 {
+
+    private static void method(int value) {
+        System.out.println("Before = " + value--);
+        if (value != 0){
+            method(value);
+        }
+        System.out.println("After = " + value);
+    }
+
+    public static void main(String[] args) {
+        method(5);
+    }
+}
