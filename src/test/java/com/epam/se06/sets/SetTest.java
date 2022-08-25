b66bde71df0eed1d727cd4750c6c0787e3875e41
AnechAb <abubekirova@inbox.ru>
2022-08-25 14:37:32 +0300

    Code from unrecorded Lesson

--- src/test/java/com/epam/se06/sets/SetTest.java
+++ src/test/java/com/epam/se06/sets/SetTest.java
@@ -5,12 +5,17 @@ import org.junit.jupiter.api.Test;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class SetTest {
+
+    //    @Test(expected = StackOverflowError.class) // JUnit4 API
     @Test
-    void name() {
+    void hashMapQuiz() {
         //StackOverflowError
-        Set<Object> set = new HashSet<>();
+
+        Set<Object> set = new HashSet<>(32,2);
         set.add(set);
-        System.out.println(set.contains(set));
+        assertThrows(StackOverflowError.class, () -> set.contains(set));
     }
 }
